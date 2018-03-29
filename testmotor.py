03714dbec880cc0e71204761b1bdc95c173ab525
Yushi Hu <huyushi98@outlook.com>
2018-03-29 00:05:33 -0500

    better sample
    better sample

--- testmotor.py
+++ testmotor.py
@@ -2,23 +2,36 @@
 import os
 import serial
 import sys
-import numpy as np
-import motor
+import motor # this is the motor library
 
 global motorSer
-global motorId
 
+# enter the position EleksMaker is mounted
 motorSer = "/dev/ttyUSB0"
-
-#ser = serial.Serial(port='/dev/ttyUSB0',baudrate=115200)
 
 
 def main():
+
+    # define the motor class
     mot = motor.Motor(motorSer)
+
+    # open the serial and test connection
     mot.motorOpen()
+
+    # set current motor position as orginal point
     mot.setOrigin()
-    mot.takeCor(8,8)
+
+    # set the step length to 2cm
+    mot.redefineStep(2)
+
+    # let the stage move to coordinate (4,4).
+    # The program will inform when the movement is finished
+    mot.takeCor(4,4)
+
+    # return the current position of the stage
     mot.returnPos()
+
+    # close the serial
     mot.motorClose()
 
 if __name__ == "__main__": main()
